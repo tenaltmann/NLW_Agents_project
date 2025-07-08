0f96d8c971cc47477fdc33db2b9f0ee862c62e8a
Ricardo Altmann <tenaltmann@gmail.com>
2025-07-07 23:58:07 -0300

    Criação server.ts

---
+++ aulas/server/src/server.ts
@@ -0,0 +1,22 @@
+import { fastify } from 'fastify'
+import {
+    serializerCompiler,
+    validatorCompiler,
+    type ZodTypeProvider,
+
+} from 'fastify-type-provider-zod'
+
+import { fastifyCors } from '@fastify/cors'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+    origin: 'http://localhost:5173',
+})
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.listen({ port: 3333}).then(() => {
+    console.log('HTTP server running! ')
+})
